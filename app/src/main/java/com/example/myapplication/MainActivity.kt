6b8e8c6d93c3384d7651602fa33477c2e0d34b90
Evgenii Uvarov <e.uvarov@life-pay.ru>
2023-12-21 16:32:00 +0300

    init init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,38 @@
+package com.example.myapplication
+
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button = findViewById(R.id.button)
         webView = findViewById(R.id.webView)
 
-        // Enable JavaScript (optional)
-        val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = MyWebViewClient()
+    }
 
-        // Load a web page with a GET request
-        val url = "https://www.example.com"
+    fun openUrlInWebView(view: android.view.View) {
+        val url = "https://example.com"
         webView.loadUrl(url)
+        webView.visibility = android.view.View.VISIBLE
+    }
+
+    private inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            view.loadUrl(request.url.toString())
+            return true
+        }
     }
 }
